727274ad6d1f0957b4f2c0553ea68028f82fe13a
PierreTL <pierre.tampon@gmail.com>
2023-07-17 13:53:56 +0200

    Exemple de formulaire de requête POST

--- index.php
+++ index.php
@@ -64,6 +64,61 @@ $allCategorie = json_decode(file_get_contents("http://localhost/pensine/api/allC
             <?php endforeach; ?>
         </ul>
     <?php } ?>
+    <hr>
+
+    <h2>Formulaire</h2>
+    <?php
+        $apiPost = 'http://localhost/pensine/api/post';
+
+        $titre =        isset($_POST['titre']) ? $_POST['titre'] : "";
+        $url =          isset($_POST['url']) ? $_POST['url'] : "";
+        $note =         isset($_POST['note']) ? $_POST['note'] : "";
+        $tag =          isset($_POST['tag']) ? $_POST['tag'] : "";
+        $categories =   isset($_POST['categories']) ? $_POST['categories'] : "";
+        
+        $data = array(
+            'titre' =>  $titre, 
+            'url' => $url, 
+            'note' => $note, 
+            'tag' => $tag, 
+            'categories' => $categories
+        );
+
+        if ( !empty($titre) || !empty($url) || !empty($note) || !empty($tag) || !empty($categories) ) {
+            print_r($data);
+
+            $options = array(
+                'http' => array(
+                    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                    'method'  => 'POST',
+                    'content' => http_build_query($data)
+                )
+            );
+            $context  = stream_context_create($options);
+            $result = file_get_contents($apiPost, false, $context);
+
+            header("location:./");
+        }
+
+    ?>
+    <form method="post">
+        <label for="titre">Titre</label>
+        <input type="text" name="titre" value="">
+        <br>
+        <label for="url">URL</label>
+        <input type="text" name="url" value="">
+        <br>
+        <label for="note">Note</label>
+        <input type="text" name="note" value="">
+        <br>
+        <label for="tag">Tag</label>
+        <input type="text" name="tag" value="">
+        <br>
+        <label for="categories">Categorie</label>
+        <input type="text" name="categories" value="">
+        <br>
+        <button type="submit" id="titreBtn">Envoyer</button>
+    </form>
 
 
 </body>
